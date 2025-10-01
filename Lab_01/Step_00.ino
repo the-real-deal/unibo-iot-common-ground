d28ed9aad182984526d593425c4bc0b7731b50d1
Rebecca Scarcelli <rebeccascarcelli5@gmail.com>
2025-10-01 09:38:02 +0200

    Primo lab

---
+++ Lab_01/Step_00.ino
@@ -0,0 +1,17 @@
+#define LED_PIN 13
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("ON");
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("OFF");
+  delay(500);
+}
