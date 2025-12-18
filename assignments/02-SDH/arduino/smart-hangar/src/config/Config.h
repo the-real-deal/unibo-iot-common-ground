56644d986f6960ed49e584fd7fcc35f5da070301
elettra-ven <elettraventura27@gmail.com>
2025-12-18 19:08:32 +0100

    The sensor is still TBD, so for now it's just the prof's

--- assignments/02-SDH/arduino/smart-hangar/src/config/Config.h
+++ assignments/02-SDH/arduino/smart-hangar/src/config/Config.h
@@ -4,5 +4,13 @@
 #define DOOR_OPEN_POS 2250     // Door open position (90 degrees)
 #define DOOR_CLOSE_POS 750     // Door close position (0 degrees)
 
+#define ADC_RESOLUTION 1024.0
+#define VCC 5.0
+#define TEMP_SENSOR_OFFSET 0.5
+#define TEMP_SENSOR_SCALE 100.0
+
+#define NUM_TEMPERATURE_SAMPLES 5
+#define MIN_TEMP -40.0
+#define MAX_TEMP 125.0
 
 #endif
