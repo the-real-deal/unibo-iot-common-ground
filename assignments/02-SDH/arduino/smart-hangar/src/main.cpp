211a075383f843c08a47f2e6fa0e64aeaec50c6e
Rebecca Scarcelli <rebecca.scarcelli@studio.unibo.it>
2026-01-28 14:26:49 +0100

    DONE: main setup, takeOffTask.h and first setup of TakeOffTask.cpp;  TO DO: refining the logic of TakeOffTask.cpp, the complete logic of Landing

--- assignments/02-SDH/arduino/smart-hangar/src/main.cpp
+++ assignments/02-SDH/arduino/smart-hangar/src/main.cpp
@@ -1,6 +1,35 @@
+#include <./tasks/BlinkingTask.h>
+#include <./tasks/DisplayTask.h>
+#include <./tasks/LedTask.h>
+#include <./tasks/SweepingTask.h>
+#include <./tasks/TakeOffTask.h>
 #include <Arduino.h>
 
 void setup() {
+/**    // Hardware initialization
+    Door* door = new Door(DOOR_PIN);
+    Led* led = new Led(LED_PIN);
+    Lcd* lcd = new Lcd();
+    Pir* pir = new Pir(PIR_PIN);
+    Sonar* sonar = new Sonar(ECHO_PIN, TRIG_PIN, 30000);
+    Context* context = new Context();
+    
+    // Task creation
+    TakeOffTask* takeOffTask = new TakeOffTask(lcd, led, door, context, pir, sonar);
+    LandingTask* landingTask = new LandingTask(lcd, led, door, context, pir, sonar);
+    
+    // Scheduler setup
+    Scheduler scheduler;
+    scheduler.init(100);  // 100ms base period
+    
+    // Add tasks (aperiodic, will be activated on demand)
+    takeOffTask->init();  // Aperiodic task
+    landingTask->init();  // Aperiodic task
+    
+    scheduler.addTask(takeOffTask);
+    scheduler.addTask(landingTask);
+*/
+
   Serial.begin(9600);  // Initialize serial communication at 9600 baud
 }
 
--- assignments/02-SDH/arduino/smart-hangar/src/tasks/TakeOffTask.h
+++ assignments/02-SDH/arduino/smart-hangar/src/tasks/TakeOffTask.h
@@ -1,3 +1,54 @@
+//Refactoring of TakeOffTask without using innested tasks
+#ifndef __TAKEOFF_TASK__
+#define __TAKEOFF_TASK__
+
+#include "kernel/Task.h"
+#include "context/Context.h"
+#include "devices/api/Led.h"
+#include "devices/api/Lcd.h"
+#include "devices/api/Door.h"
+#include "devices/api/Pir.h"
+#include "devices/api/Sonar.h"
+#include "kernel/Config.h"
+#include "kernel/MsgService.h"
+#include <Arduino.h>
+
+class TakeOffTask: public Task{
+
+public:
+    TakeOffTask(Lcd* pLcd, Led* pLed, Door* pDoor, Context* pContext, Pir* pDistance, Sonar* pSonar);
+    void tick();
+
+private:
+    enum State { IDLE, OPENING_DOOR, DRONE_EXITING, CLOSING_DOOR, COMPLETED };
+    State state;
+
+    void setState(State newState);
+    long elapsedTimeInState();
+    bool checkAndSetJustEntered();
+    void log(const String& msg);
+    void updateDisplay();
+    bool shouldStartTakeOff();
+
+    Lcd* pLcd;
+    Led* pLed;
+    Door* pDoor;
+    Pir* pDistance;
+    Sonar* pSonar;
+
+    Context* pContext;
+
+    long stateTimestamp;
+    bool justEntered;
+    unsigned long lastDisplayUpdate;
+};
+
+#endif
+
+
+
+
+/*
 #ifndef __TAKEOFF_TASK__
 #define __TAKEOFF_TASK__
 
@@ -39,4 +90,4 @@ private:
   bool justEntered;
 };
 
-#endif
+#endif*/
