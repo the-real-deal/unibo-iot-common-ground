99b791f892c7c0750aa1cae78dd18dbcf9365cfb
elettra-ven <elettraventura27@gmail.com>
2026-02-06 09:33:27 +0100

    +

---
+++ assignments/02-SDH/arduino/smart-hangar/src/tasks/api/HangarDoorTask.h
@@ -0,0 +1,45 @@
+#ifndef __HANGAR_DOOR_TASK__
+#define __HANGAR_DOOR_TASK__
+
+#include "kernel/Task.h"
+#include "context/Context.h"
+#include "devices/api/Led.h"
+#include "devices/api/Lcd.h"
+#include "devices/api/Door.h"
+#include "devices/api/Pir.h"
+#include "devices/api/Sonar.h"
+#include "kernel/Config.h"
+#include "kernel/MsgService.h"
+#include <Arduino.h>
+
+class HangarDoorTask: public Task{
+
+public:
+    HangarDoorTask(Lcd* pLcd, Led* pLed, Door* pDoor, Context* pContext, Sonar* pSonar);
+
+    void tick();
+
+private:
+    enum State { CLOSED, OPENING_DOOR, OPEN, CLOSING_DOOR };
+    State state;
+
+    void openDoor();
+    void closeDoor();
+    void setState(State newState);
+    long elapsedTimeInState();
+    bool checkAndSetJustEntered();
+
+    Lcd* pLcd;
+    Led* pLed;
+    Door* pDoor;
+    Pir* pDistance;
+    Sonar* pSonar;
+
+    Context* pContext;
+
+    long stateTimestamp;
+    bool justEntered;
+    unsigned long lastDisplayUpdate;
+};
+
+#endif
